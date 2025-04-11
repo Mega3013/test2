ff46c52ab97c6055739d89866d24b3e14ab1dc0b
Mega3013 <158750049+Mega3013@users.noreply.github.com>
2025-04-11 06:51:54 +0400

    Update custom-fixtures.ts

--- fixtures/custom-fixtures.ts
+++ fixtures/custom-fixtures.ts
@@ -1,12 +1,28 @@
-// fixtures/custom-fixtures.ts
-import { test as baseTest } from '@playwright/test';
+import { test as baseTest, BrowserContext } from '@playwright/test';
 
-export const test = baseTest.extend({
-  page: async ({ page }, use) => {
+export const test = baseTest.extend<{
+  context: BrowserContext;
+}>({
+  context: async ({ browser }, use) => {
+    const context = await browser.newContext({
+      userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36',
+      extraHTTPHeaders: {
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+        'Accept-Language': 'en-US,en;q=0.9',
+        'Cache-Control': 'no-cache',
+        'Pragma': 'no-cache',
+        'Upgrade-Insecure-Requests': '1',
+      },
+    });
+    await use(context);
+    await context.close();
+  },
+
+  page: async ({ context }, use) => {
+    const page = await context.newPage();
     await page.goto('');
-    await page.waitForTimeout(1000);
+    await page.waitForTimeout(900);
 
-    // Ждем, если баннер появляется
     const closeBtn = page.locator('button[data-dismiss="modal"]', {
       hasText: 'Shop now!',
     });
@@ -19,8 +35,8 @@ export const test = baseTest.extend({
       await closeBtn.first().click();
     }
 
-    // Передаем управление дальше
     await use(page);
+    await page.close();
   },
 });
 
